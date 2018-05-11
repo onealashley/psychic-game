18585e7655aa99e66949a58db3c80473ded94992
ashley o <oneal.ashleyc@gmail.com>
2018-05-11 15:47:31 -0400

    first commit

---
+++ assets/game.js
@@ -0,0 +1,47 @@
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var win = 0;
+    var losses = 0;
+    var guesses = 10;
+    var guessesSoFar = [];
+
+    var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+    console.log(computerChoice);
+
+
+    document.onkeyup = function(event) {
+        var userGuess = event.key;
+        guessesSoFar[guessesSoFar.length]=userGuess;
+		
+        if (userGuess == computerChoice) {
+            win = win + 1;
+            document.getElementById("wins").innerHTML = "Wins: " + win;
+            swal("You Win", "The Letter was " + computerChoice, "success", {
+                button:"High Five!!",});
+            computerChoice = letters[Math.floor(Math.random() * letters.length)];
+            guesses = 10;
+            guessesSoFar = [];
+            document.getElementById("guessed").innerHTML = "Guessed so far:";
+            document.getElementById("guesses").innerHTML = "Guesses left: 10";
+            
+
+        } else {
+            guesses = guesses - 1;
+            document.getElementById("guesses").innerHTML = "Guesses Left: " + guesses;
+            document.getElementById("guessed").innerHTML = "Guessed so far: " + guessesSoFar;
+
+        }
+
+        if (guesses == 0) {
+            losses = losses + 1;
+            document.getElementById("losses").innerHTML = "Losses: " + losses;
+            swal("You Lose", "The Letter Was " + computerChoice, "warning", {
+                button:"Try Again :("
+            });
+            computerChoice = letters[Math.floor(Math.random() * letters.length)];
+            guesses = 10;
+            guessesSoFar = [];
+            document.getElementById("guessed").innerHTML = "Guessed so far:";
+            document.getElementById("guesses").innerHTML = "Guesses left: 10";            
+        }
+    }
+
