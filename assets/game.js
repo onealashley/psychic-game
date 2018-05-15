f3665434957d4dcd768992cb1a6c10b3a8283a28
ashley o <oneal.ashleyc@gmail.com>
2018-05-15 15:10:26 -0400

    final commit

--- assets/game.js
+++ assets/game.js
@@ -1,53 +1,56 @@
     
     
-    function reset() {
-        computerChoice = letters[Math.floor(Math.random() * letters.length)];
-        console.log(computerChoice)
-        guesses = 10;
-        guessesSoFar = [];
-        document.getElementById("guessed").innerHTML = "Guessed so far:";
-        document.getElementById("guesses").innerHTML = "Guesses left: 10";
-    }
-    
-    
-    
-    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var win = 0;
-    var losses = 0;
-    var guesses = 10;
-    var guessesSoFar = [];
+function reset() {
+    shouldContinue = true;
+    computerChoice = letters[Math.floor(Math.random() * letters.length)];
+    console.log(computerChoice)
+    guesses = 10;
+    guessesSoFar = [];
+    document.getElementById("guessed").innerHTML = "Guessed so far:";
+    document.getElementById("guesses").innerHTML = "Guesses left: 10";
+}
 
-    var computerChoice = letters[Math.floor(Math.random() * letters.length)];
-    console.log(computerChoice);
 
-    function start() {
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var win = 0;
+var losses = 0;
+var guesses = 10;
+var guessesSoFar = [];
+var shouldContinue = false;
+
+var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+console.log(computerChoice);
+
+function start() {
+    shouldContinue = true;
+    
     document.onkeyup = function(event) {
-        var userGuess = event.key;
-        guessesSoFar[guessesSoFar.length]=userGuess;
-		
-        if (userGuess == computerChoice) {
-            win = win + 1;
-            document.getElementById("wins").innerHTML = "Wins: " + win;
-            swal("You Win", "The Letter was " + computerChoice, "success", {
-                button:"High Five!!",});
-                 
-
-        } else {
-            guesses = guesses - 1;
-            document.getElementById("guesses").innerHTML = "Guesses Left: " + guesses;
-            document.getElementById("guessed").innerHTML = "Guessed so far: " + guessesSoFar;
-
+        if (shouldContinue === true) {
+            var userGuess = event.key;
+            guessesSoFar[guessesSoFar.length]=userGuess;
+            
+            if (userGuess == computerChoice) {
+                win = win + 1;
+                shouldContinue = false;
+                document.getElementById("wins").innerHTML = "Wins: " + win;
+                swal("You Win", "The Letter was " + computerChoice, "success", {
+                    button:"High Five!!",});
+            } else {
+                guesses = guesses - 1;
+                document.getElementById("guesses").innerHTML = "Guesses Left: " + guesses;
+                document.getElementById("guessed").innerHTML = "Guessed so far: " + guessesSoFar;
+            }
+    
+            if (guesses == 0) {
+                losses = losses + 1;
+                shouldContinue = false;
+                document.getElementById("losses").innerHTML = "Losses: " + losses;
+                swal("You Lose", "The Letter Was " + computerChoice, "warning", {
+                    button:"Try Again :("});
+            }
         }
-
-        if (guesses == 0) {
-            losses = losses + 1;
-            document.getElementById("losses").innerHTML = "Losses: " + losses;
-            swal("You Lose", "The Letter Was " + computerChoice, "warning", {
-                button:"Try Again :("
-            });
-                             
-        }
-
-    } }
+    }   
+} 
     
         
